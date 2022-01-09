8fde21388f8a24701c413ce67967473e8fdb29df
Evgeny Kislov <dev@evgenykislov.com>
2022-01-09 22:56:40 +0300

    Redirects empty path to localsrv admin view.

--- doxysearch/urls.py
+++ doxysearch/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url = "/localsrv/admin/")),
     path('admin/', admin.site.urls),
     path("localsrv/", include("localsrv.urls"))
 ]
